7f26de3ae46768c5aad4df7e1dd63d79e3e4de87
MohamedBan <114017198+MohamedBan@users.noreply.github.com>
2024-11-05 14:42:21 -0600

    implemted cue scene

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -15,6 +15,7 @@ func _ready() -> void:
 func new_game():
 	generate_balls()
 	reset_cue_ball()
+	show_cue()
 	
 func load_images():
 	for i in range(1,  17, 1):
@@ -52,5 +53,9 @@ func reset_cue_ball():
 	cue_ball.position = START_POS
 	cue_ball.get_node("Sprite2D").texture = ball_images.back()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func show_cue():
+	$Cue.position = cue_ball.position
+
 func _process(delta) -> void:
 	pass
