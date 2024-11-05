5217f58a0be4b0e3e8d383c9dd8fecc45759c6fb
MohamedBan <114017198+MohamedBan@users.noreply.github.com>
2024-11-05 14:22:45 -0600

    test commit

--- Scenes/powerup.gd
+++ Scenes/powerup.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+#asks a signal then ask for as randomized
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
