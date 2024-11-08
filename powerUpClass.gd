b6e32eaeb01e3cca8d2c7b94fd1e314984435a67
CompAj <114016409+CompAj@users.noreply.github.com>
2024-11-08 00:26:02 -0600

    Creating parent class for power-ups

---
+++ powerUpClass.gd
@@ -0,0 +1,13 @@
+class_name PowerUp
+
+var name: String
+var description: String 
+var effect: String
+
+func _init(powerName: String, powerEffect: String): 
+	self.name = powerName
+	self.effect = effect
+	
+func apply_effect(): 
+	pass 
+ 
