793bbc81f3c48bb9bf536991d27cf1b295e67dc4
CompAj <114016409+CompAj@users.noreply.github.com>
2024-11-07 19:01:07 -0600

    Chance of Getting a power up
    - Every time you pot a ball you will have a 40% chance of getting a new power up

--- Scenes/powerup.gd
+++ Scenes/powerup.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
-
+var rng = RandomNumberGenerator.new() 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$"..".connect("power_gamble", Callable(self, "power_draw"))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-func power_draw() -> void: 
-	print("Ball has gone in")
+func power_draw() -> void:
+	var rand_num = rng.randi_range(0, 10)
+	if rand_num < 4: 
+		power_selector()
+	print(rand_num)
+
+
+func power_selector():
+	print("Random power is being drawn...")
+	# Place where all the different power ups will go 
