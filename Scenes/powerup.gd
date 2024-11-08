d5cf5c8c6bc64b576bf80a388c0e4f2c2ff72d9e
CompAj <114016409+CompAj@users.noreply.github.com>
2024-11-07 21:28:21 -0600

    Temp Power Up Factory
    - Used enums to make power up factory
    - The factory will eventually create power-ups and add it to the player inventory

--- Scenes/powerup.gd
+++ Scenes/powerup.gd
@@ -1,17 +1,36 @@
 extends Node2D
 
-var rng = RandomNumberGenerator.new() 
+var rng = RandomNumberGenerator.new()
+enum PowerUps { TELEPORT, WALL, MRBIG, TORNADO, REWIND, GHOST }
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"..".connect("power_gamble", Callable(self, "power_draw"))
+	get_parent().connect("power_gamble", Callable(self, "power_draw"))
 
 func power_draw() -> void:
 	var rand_num = rng.randi_range(0, 10)
+	print(rand_num)
 	if rand_num < 4: 
 		power_selector()
-	print(rand_num)
-
+	
 
 func power_selector():
 	print("Random power is being drawn...")
-	# Place where all the different power ups will go 
+	var rand_num = rng.randi_range(0, 5)
+	power_up_factory(rand_num)  
+
+func power_up_factory(power_up: int):
+	
+	match power_up:
+		PowerUps.TELEPORT:
+			print("Teleport power-up created")
+		PowerUps.WALL:
+			print("Wall power-up created")
+		PowerUps.MRBIG:
+			print("MrBig power-up created")
+		PowerUps.TORNADO:
+			print("Tornado power-up created")
+		PowerUps.REWIND:
+			print("Rewind power-up created")
+		PowerUps.GHOST:
+			print("Ghost power-up created")
