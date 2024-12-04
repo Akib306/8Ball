d97710971737effc1e3548acab9a1279cc9373a0
kararAcode <63989704+kararAcode@users.noreply.github.com>
2024-12-03 22:22:50 -0600

    comment out unused powerups for now in factory

--- Scripts/power_up_factory.gd
+++ Scripts/power_up_factory.gd
@@ -1,24 +1,26 @@
 extends Node
 class_name PowerUpFactory
 
-enum PowerUps {TELEPORT, WALL, MRBIG, TORNADO, REWIND, GHOST}
+enum PowerUps {MRBIG}
 
 
 # Create a power-up based on its type/index
 static func create_power_up(power_up_type: int) -> PowerUp:
 	match power_up_type:
-		PowerUps.TELEPORT:
-			return TeleportPowerUp.new()
-		PowerUps.WALL:
-			return WallPowerUp.new()
 		PowerUps.MRBIG:
 			return MrBigPowerUp.new()
-		PowerUps.TORNADO:
-			return TornadoPowerUp.new()
-		PowerUps.REWIND:
-			return RewindPowerUp.new()
-		PowerUps.GHOST:
-			return GhostPowerUp.new()
+		#PowerUps.TELEPORT:
+			#return TeleportPowerUp.new()
+		#PowerUps.WALL:
+			#return WallPowerUp.new()
+		#PowerUps.MRBIG:
+			#return MrBigPowerUp.new()
+		#PowerUps.TORNADO:
+			#return TornadoPowerUp.new()
+		#PowerUps.REWIND:
+			#return RewindPowerUp.new()
+		#PowerUps.GHOST:
+			#return GhostPowerUp.new()
 		_:
 			print("Unknown power-up type!")
 			return null
