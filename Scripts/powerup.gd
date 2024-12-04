2a103078be618d1c72370dce11690f954114f47e
kararAcode <63989704+kararAcode@users.noreply.github.com>
2024-12-03 20:36:18 -0600

    Create powerup.gd

---
+++ Scripts/powerup.gd
@@ -0,0 +1,14 @@
+class_name PowerUp
+extends Node
+
+enum EffectType { INSTANT, TURN_BASED }
+var effect_type: EffectType = EffectType.INSTANT
+var powerupOwner: Player = null  # The player who owns/activates this power-up
+
+# Called when the power-up is activated
+func activate():
+	pass
+
+# Called at the end of the owner's turn (only for turn-based power-ups)
+func on_turn_end():
+	pass
